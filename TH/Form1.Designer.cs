92730e9f0e8ab69d7dac8da9c7434cfa3d911c42
Hariprasd Navilur N <hariprasadnavilurn@gmail.com>
2021-02-23 22:47:29 +0530

    adding missing pieces

---
+++ TH/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.heading = new System.Windows.Forms.Label();
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.qnumLabel = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // heading
+            // 
+            this.heading.AutoSize = true;
+            this.heading.Font = new System.Drawing.Font("Tempus Sans ITC", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heading.ForeColor = System.Drawing.Color.Navy;
+            this.heading.Location = new System.Drawing.Point(500, 10);
+            this.heading.Name = "heading";
+            this.heading.Size = new System.Drawing.Size(281, 42);
+            this.heading.TabIndex = 0;
+            this.heading.Text = "TREASURE HUNT";
+            // 
+            // answerBox
+            // 
+            this.answerBox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerBox.Location = new System.Drawing.Point(507, 683);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.Size = new System.Drawing.Size(200, 27);
+            this.answerBox.TabIndex = 2;
+            this.answerBox.TextChanged += new System.EventHandler(this.answerBox_TextChanged);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(800, 683);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 27);
+            this.submitButton.TabIndex = 3;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Segoe UI Emoji", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(295, 685);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(46, 25);
+            this.timeLabel.TabIndex = 4;
+            this.timeLabel.Text = "0:00";
+            // 
+            // qnumLabel
+            // 
+            this.qnumLabel.AutoSize = true;
+            this.qnumLabel.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.qnumLabel.Location = new System.Drawing.Point(157, 75);
+            this.qnumLabel.Name = "qnumLabel";
+            this.qnumLabel.Size = new System.Drawing.Size(0, 24);
+            this.qnumLabel.TabIndex = 5;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(240, 60);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 741);
+            this.Controls.Add(this.qnumLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.heading);
+            this.Name = "Form1";
+            this.Text = "Hunt for pleasure";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label heading;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label qnumLabel;
+    }
+}
+
---
+++ TH/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TH
+{
+    public partial class Form1 : Form
+    {
+        const int NUM_QUES = 14;
+        string[] answers = new string[] { "start", "ironmaiden", "abdevilliers", "mozilla", "sexpositions", "harrypotter", "elonmusk", "robertdowneyjunior",
+                                "lalbahadurshastri", "osamabinladen", "cristianoronaldo", "khatronkekhiladi", "emraanhashmi", "dubsmash", "iamsherlocked"};
+        List<Image> images = new List<Image>();
+        String query = ""; // answer submitted by the player
+        static int qnum = 0; // current question numbers
+        enum STATE {RUNNING, ENDED};
+        STATE state = STATE.RUNNING;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            images.Add(TH.Properties.Resources.i0);
+            images.Add(TH.Properties.Resources.i1);
+            images.Add(TH.Properties.Resources.i2);
+            images.Add(TH.Properties.Resources.i3);
+            images.Add(TH.Properties.Resources.i4);
+            images.Add(TH.Properties.Resources.i5);
+            images.Add(TH.Properties.Resources.i6);
+            images.Add(TH.Properties.Resources.i7);
+            images.Add(TH.Properties.Resources.i8);
+            images.Add(TH.Properties.Resources.i9);
+            images.Add(TH.Properties.Resources.i10);
+            images.Add(TH.Properties.Resources.i11);
+            images.Add(TH.Properties.Resources.i12);
+            images.Add(TH.Properties.Resources.i13);
+            images.Add(TH.Properties.Resources.i14);
+            images.Add(TH.Properties.Resources.i15);
+
+            pictureBox.Image = images[0];
+            qnumLabel.Text = qnum.ToString();
+            CheckAnswer();
+            timer1.Start();
+        }
+
+        private void CheckAnswer()
+        {
+            if (state == STATE.RUNNING)
+            {
+                if (answers[qnum] == query) // check correctness of answer
+                {
+                    qnum++; // if correct, goto next image
+                    pictureBox.Image = images[qnum];
+                    seconds = minutes = 0;
+                    qnumLabel.Text = qnum.ToString();
+
+                    if (qnum > NUM_QUES) // if the questions are over...
+                    {
+                        state = STATE.ENDED;
+                        submitButton.Enabled = false;
+                        answerBox.Text = DateTime.Now.ToString();
+                        answerBox.ReadOnly = true;
+                        timer1.Stop();
+                        timeLabel.Text = "0:00";
+                        return;
+                    }
+                }
+
+                answerBox.Text = ""; // clear the previous answer.
+                query = ""; // should be after the end of if
+            }
+        }
+
+        private void answerBox_TextChanged(object sender, EventArgs e)
+        {
+            query = answerBox.Text;
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            CheckAnswer();
+        }
+
+        int seconds = 0, minutes = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            if (seconds == 60)
+            {
+                minutes++;
+                seconds = 0;
+            }
+            timeLabel.Text = minutes.ToString() + ":" + seconds.ToString();
+        }
+    }
+}
